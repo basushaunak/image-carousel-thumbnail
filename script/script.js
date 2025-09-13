055c83c9725ea3b10a5e72db9eb8a89ba414b301
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-09-13 18:16:51 +0530

    Initial App done

--- script/script.js
+++ script/script.js
@@ -1,1 +1,183 @@
-// JS Scripts
+const demoImages = [
+  {
+    url: "./assets/images/1.jpg",
+    title: "dhuandhar falls",
+    category: "nature",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/2.jpg",
+    title: "backwater sunset",
+    category: "nature",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/3.jpg",
+    title: "white daisy",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/4.jpg",
+    title: "sand artist",
+    category: "art",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/5.jpg",
+    title: "wild flower",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/6.jpg",
+    title: "boats in the dock",
+    category: "profession",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/7.jpg",
+    title: "zinnia flower",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/8.jpg",
+    title: "boy and the sea",
+    category: "nature",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/9.jpg",
+    title: "white daisies",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/10.jpg",
+    title: "fishing in the sea",
+    category: "profession",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/11.jpg",
+    title: "daisy and moth",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/12.jpg",
+    title: "sail boat",
+    category: "profession",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/13.jpg",
+    title: "red rose",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/14.jpg",
+    title: "hills and valley",
+    category: "nature",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/15.jpg",
+    title: "champak",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/16.jpg",
+    title: "himalayas",
+    category: "nature",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/17.jpg",
+    title: "petunia",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/18.jpg",
+    title: "himalayan roads",
+    category: "nature",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/19.jpg",
+    title: "royal poinciana",
+    category: "flower",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+  {
+    url: "./assets/images/20.jpg",
+    title: "frozen lake",
+    category: "nature",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque delectus consequuntur repellendus odio incidunt laborum commodi minima et officiis beatae labore quae quaerat, assumenda cupiditate quidem dicta reprehenderit quis.",
+    altText: "",
+  },
+];
+
+function init(images = demoImages){
+  let activeImage = 0;
+  const imageCarousel = document.querySelector(".image-carousel");
+  const primaryImage = document.querySelector("#primary-image");
+  for(let i = 0;i<images.length;i++){
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("image-carousel-imageholder");
+    newDiv.id = `div-${i}`.padStart(3,0);
+    let newImg = document.createElement("img");
+    newImg.src = images[i].url;
+    newImg.alt = images[i].alt;
+    newDiv.appendChild(newImg);
+    imageCarousel.appendChild(newDiv);
+  }
+  setActiveImage(activeImage,0);
+  document.querySelector(".image-carousel").addEventListener("click",(e)=>{
+    if(e.target.tagName === "IMG"){
+      let clickedDiv = e.target.parentElement;
+      let newActive = parseInt(clickedDiv.id.substring(4));
+      activeImage = setActiveImage(activeImage,newActive);
+    }
+  })
+  function setActiveImage(oldActive,newActive){
+    let oldId = `div-${oldActive}`.padStart(3,0);
+    let newId = `div-${newActive}`.padStart(3,0);
+    document.querySelector(`#${oldId}`).classList.remove("active-image");
+    let newActiveElement = document.querySelector(`#${newId}`);
+    newActiveElement.classList.add("active-image");
+    imageCarousel.appendChild(newActiveElement);
+    primaryImage.src = images[newActive].url;
+    document.querySelector(".text-content h1").innerText = images[newActive].title;
+    document.querySelector(".text-content h2").innerText = images[newActive].category;
+    document.querySelector(".text-content p").innerText = images[newActive].description;
+    return newActive;
+  }
+}
+
+init(demoImages);
+
+// e.target.classList.contains("carousel-item")
